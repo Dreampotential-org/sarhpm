8da6e25feef7ea47cf6c965d9ebdf7da33508922
Aaron Rosen <aaronorosen@gmail.com>
2018-12-27 12:12:54 -0800

    initial working

---
+++ dprojx/static/custom.js
@@ -0,0 +1,36 @@
+$('#signin').on('click', function(e) {
+  e.preventDefault();
+  $('#signinModal').addClass('is-visible');
+});
+$('#signup').on('click', function(e) {
+  e.preventDefault();
+  $('#signupModal').addClass('is-visible');
+});
+$('#locationAuth').on('click', function(e) {
+  e.preventDefault();
+  $('#LocationModal').addClass('is-visible');
+});
+$('.modal-overlay').on('click', function(e) {
+  $('.modal').removeClass('is-visible');
+});
+$('.toggleBar').on('click', function(e) {
+  $('.slideMenu').toggle();
+    $(this).toggleClass('toggleClose');
+});
+
+
+//For Firefox we have to handle it in JavaScript 
+var vids = $("video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+//Loop though all Video tags and set Controls as false
+
+$("video").click(function() {
+  //console.log(this); 
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
