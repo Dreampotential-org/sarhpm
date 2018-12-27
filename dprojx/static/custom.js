4ca52bab6f160f51c9275944cc186e3c32e709a8
Aaron Rosen <aaronorosen@gmail.com>
2018-12-27 12:17:25 -0800

    more

--- dprojx/static/custom.js
+++ dprojx/static/custom.js
@@ -1,36 +1,41 @@
-$('#signin').on('click', function(e) {
-  e.preventDefault();
-  $('#signinModal').addClass('is-visible');
-});
-$('#signup').on('click', function(e) {
-  e.preventDefault();
-  $('#signupModal').addClass('is-visible');
-});
-$('#locationAuth').on('click', function(e) {
-  e.preventDefault();
-  $('#LocationModal').addClass('is-visible');
-});
-$('.modal-overlay').on('click', function(e) {
-  $('.modal').removeClass('is-visible');
-});
-$('.toggleBar').on('click', function(e) {
-  $('.slideMenu').toggle();
-    $(this).toggleClass('toggleClose');
-});
+function init_all() {
+
+    $('#signin').on('click', function(e) {
+      e.preventDefault();
+      $('#signinModal').addClass('is-visible');
+    });
+    $('#signup').on('click', function(e) {
+      e.preventDefault();
+      $('#signupModal').addClass('is-visible');
+    });
+    $('#locationAuth').on('click', function(e) {
+      e.preventDefault();
+      $('#LocationModal').addClass('is-visible');
+    });
+    $('.modal-overlay').on('click', function(e) {
+      $('.modal').removeClass('is-visible');
+    });
+    $('.toggleBar').on('click', function(e) {
+      $('.slideMenu').toggle();
+        $(this).toggleClass('toggleClose');
+    });
 
 
-//For Firefox we have to handle it in JavaScript 
-var vids = $("video"); 
-$.each(vids, function(){
-       this.controls = false; 
-}); 
-//Loop though all Video tags and set Controls as false
+    //For Firefox we have to handle it in JavaScript
+    var vids = $("video");
+    $.each(vids, function(){
+           this.controls = false;
+    });
+    //Loop though all Video tags and set Controls as false
 
-$("video").click(function() {
-  //console.log(this); 
-  if (this.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-});
+    $("video").click(function() {
+      //console.log(this);
+      if (this.paused) {
+        this.play();
+      } else {
+        this.pause();
+      }
+    });
+
+}
+window.addEventListener("DOMContentLoaded", init_all, false);
