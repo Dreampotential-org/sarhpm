e43bf9b5a30299ea125d38569fe95a5d431437da
Aaron Rosen <aaronorosen@gmail.com>
2019-01-31 10:47:29 -0800

    try new command

--- dprojx/dappx/views.py
+++ dprojx/dappx/views.py
@@ -117,10 +117,15 @@ def post_slack_errors(request):
 
 def convert_file(uploaded_file_url):
     outfile = "%s.mp4" % uploaded_file_url.rsplit(".", 1)[0]
+
     command = (
-        'ffmpeg -i ./%s -vcodec copy -acodec copy ./%s'
-        % (uploaded_file_url, outfile)
-    )
+        "ffmpeg -an -i %s -vcodec libx264 -codec:a libmp3lame "
+        "-qscale:a 1 -pix_fmt yuv420p -profile:v baseline "
+        "-level 3 %s" % (uploaded_file_url, outfile))
+    #command = (
+    #    'ffmpeg -i ./%s -vcodec copy -acodec copy ./%s'
+    #    % (uploaded_file_url, outfile)
+    #)
     print (command)
     os.system(command)
     return outfile
