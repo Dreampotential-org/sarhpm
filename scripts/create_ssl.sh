7183e55b90db25ee050d8fea6933bec95b56781d
Ubuntu <ubuntu@ip-172-31-27-154.us-east-2.compute.internal>
2019-03-12 01:48:29 +0000

    more

--- scripts/create_ssl.sh
+++ scripts/create_ssl.sh
@@ -2,4 +2,4 @@
 
 /usr/local/bin/docker-compose -f docker-compose-staging.yml run certbot \
     certonly --webroot --webroot-path=/var/www/html --email aaronorosen@gmail.com \
-    --agree-tos --no-eff-email --force-renewal -d beachcomber.usepam.com
+    --agree-tos --no-eff-email --force-renewal -d app.usepam.com
