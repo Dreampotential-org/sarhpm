9b81305e7782a2e213a8e756aa6f87cc721a0ca0
adem oztas <adem@oztas.net>
2019-04-14 21:13:25 +0300

    web and certbox

--- scripts/create_ssl.sh
+++ scripts/create_ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f docker-compose-staging.yml run certbot \
+/usr/local/bin/docker-compose -f docker-compose-web.yml run certbot \
     certonly --webroot --webroot-path=/var/www/html --email aaronorosen@gmail.com \
-    --agree-tos --no-eff-email --force-renewal -d app.usepam.com
+    --agree-tos --no-eff-email --force-renewal -d $1 
