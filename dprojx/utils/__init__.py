ee590433e65add6412cd19ada775ae616c359e02
Aaron Rosen <aaronorosen@gmail.com>
2019-05-05 14:00:11 -0700

    fix login link

--- dprojx/utils/__init__.py
+++ dprojx/utils/__init__.py
@@ -5,6 +5,6 @@ def superuser_only(function):
     def _inner(request, *args, **kwargs):
         if not request.user.is_superuser:
             return HttpResponse(
-                'Admin access required <a href="/login">Login</a>')
+                'Admin access required <a href="/admin">Admin Login</a>')
         return function(request, *args, **kwargs)
     return _inner
