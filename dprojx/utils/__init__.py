8f3a1d89eadcd23219ec519f71635e4247a733e6
Aaron Rosen <aaronorosen@gmail.com>
2019-05-05 13:57:46 -0700

    provide login link on admin access required screen

--- dprojx/utils/__init__.py
+++ dprojx/utils/__init__.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 def superuser_only(function):
     def _inner(request, *args, **kwargs):
         if not request.user.is_superuser:
-            return HttpResponse('Admin access required')
+            return HttpResponse(
+                'Admin access required <a href="/login">Login</a>')
         return function(request, *args, **kwargs)
     return _inner
