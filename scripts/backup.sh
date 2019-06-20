551ef283bf2fcb800a8ba6c1be8dc4c8a06161cc
adem oztas <adem@oztas.net>
2019-06-20 16:15:28 +0300

    fixed backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo tar cvfz /opt/var/backup/backup-$(date '+%Y-%m-%d-%H-%M').tar.gz /opt/var/media
-sudo docker exec -t deploy-prod-pam_db_1 pg_dumpall -c -U postgres > /opt/var/backup/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+sudo docker exec -t deploy-prod_db_1 pg_dumpall -c -U postgres > /opt/var/backup/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
