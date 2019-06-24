f83f955dd87f76135bbcf57485ae76d440039ecb
adem oztas <adem@oztas.net>
2019-06-24 13:22:55 +0300

    copied staging static files

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-web.yml down 
+cp -r ../dev-useiam/dprojx/static/* ../static-staging/
+
+docker-compose -f docker-compose-web.yml down
 docker-compose -f docker-compose-web.yml build
 docker-compose -f docker-compose-web.yml up -d
 
