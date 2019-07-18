f0f4f8780b820f695a599346ca0303023e62f3e6
adem oztas <adem@oztas.net>
2019-07-19 00:46:04 +0300

    removed unused import

--- dprojx/api/views.py
+++ dprojx/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
