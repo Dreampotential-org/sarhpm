fbc0085f51c4686115a34cf3517aa80545378441
Aaron Rosen <aaronorosen@gmail.com>
2019-08-13 15:17:27 -0700

    prod backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-sudo tar cvfz /opt/var/backup/backup-$(date '+%Y-%m-%d-%H-%M').tar.gz /opt/var/media
-sudo docker exec -t deploy-prod_db_1 pg_dumpall -c -U postgres > /opt/var/backup/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+export AWS_ACCESS_KEY_ID=AKIAU7EQAGZOF7PQ2HER
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-2
+
+# create backup dir
+BACKUP_DIR=/opt/var/backup/backup-$(date '+%Y-%m-%d-%H-%M')
+mkdir $BACKUP_DIR
+
+# backup prod-api
+sudo tar cvfz $BACKUP_DIR/backup-media-prod-api-$(date '+%Y-%m-%d-%H-%M').tar.gz /opt/var/media-prod-api
+sudo docker exec -t deploy-prod_db_1 pg_dumpall -c -U postgres > $BACKUP_DIR/media-prod-api_dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+sudo tar -cvf $BACKUP_DIR.tar $BACKUP_DIR
+
+aws s3 cp $BACKUP_DIR.tar s3://backups-useiam
