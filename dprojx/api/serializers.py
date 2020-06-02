5936aaed4efb9c4c65f91a69ef7b3013d189ccc1
Aaron Rosen <aaronorosen@gmail.com>
2020-06-02 14:25:33 -0700

    fix

--- dprojx/api/serializers.py
+++ dprojx/api/serializers.py
@@ -7,7 +7,7 @@ from dappx.models import UserProfileInfo, GpsCheckin, VideoUpload
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        # fields = ['id', 'username', 'email', 'password']
         exclude = ('password',)
 
 
