1c120d21f51901001d74cae7db012345dad6771b
Aaron Rosen <aaronorosen@gmail.com>
2020-06-02 19:17:16 -0700

    Improve the new monitor text

--- dprojx/dappx/constants.py
+++ dprojx/dappx/constants.py
@@ -1,12 +1,15 @@
 existing_monitor_message = (
-    "Hi there, I have added you to monitor my recovery process "
-    "and help keep me accountable using https://useiam.com. "
+    "Hi there, I have added you my personal techno-journal which "
+    "helps keep me accountable.<br><br>"
     "You will receive video and gps checkins of my activity which "
-    "you can reply to via email to help encourage me.")
+    "you can reply to via email or through the platform. <br><br>"
+    "https://useiam.com to learn more."
+)
 
 new_monitor_message = (
-    "Hi there, I have added you to monitor my recovery process "
-    "and help keep me accountable using https://useiam.com. "
+    "Hi there, I have added you my personal techno-journal which "
+    "helps keep me accountable.<br><br>"
     "You will receive video and gps checkins of my activity which "
-    "you can reply to via email to help encourage me. "
-    )
+    "you can reply to via email or through the platform. <br><br>"
+    "https://useiam.com to learn more."
+)
