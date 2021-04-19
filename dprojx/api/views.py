d98755379927adfe093764c5e0e9ef081f51380f
Tanveer Sultan <engr.tanveersultan53.com>
2021-04-19 15:49:05 +0500

    Signup with magic link and create user if not exisit

--- dprojx/api/views.py
+++ dprojx/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from dprojx.dprojx.settings import website_url
+from dprojx.settings import website_url
 
 from api.serializers import (
     UserSerializer, UserProfileSerializer, GpsCheckinSerializer,
