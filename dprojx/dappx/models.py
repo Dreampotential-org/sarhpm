7bdfdd4d1d7396b98dc95e9e80bde359e34b8c23
Aaron Rosen <aaronorosen@gmail.com>
2021-06-22 12:50:14 -0700

    Add new models for orgs

--- dprojx/dappx/models.py
+++ dprojx/dappx/models.py
@@ -71,32 +71,6 @@ class VideoUpload(models.Model):
         )
 
 
-class UserProfileInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=17, blank=True , null=True)
-    name = models.CharField(max_length=17, blank=True,null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    notify_email = models.EmailField(max_length=512, blank=True, null=True)
-    days_sober = models.PositiveIntegerField(default=0, null=True)
-    sober_date = models.CharField(max_length=256, blank=True, null=True)
-    user_hash = models.CharField(max_length=256, blank=True, null=True)
-    source = models.CharField(max_length=500, default="",null=True)
-    stripe_subscription_id = models.CharField(max_length=256,
-                                              blank=True, null=True)
-    paying = models.BooleanField(default=False)
-    iap_blurb = models.TextField(default="")
-
-    def __str__(self):
-        return self.user.username
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.user_hash = hashlib.sha1(
-                self.user.email.encode('utf-8')
-            ).hexdigest()
-        super(UserProfileInfo, self).save(*args, **kwargs)
-
-
 class SubscriptionEvent(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, default="")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -107,3 +81,44 @@ class SubscriptionEvent(models.Model):
 class UserMonitor(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, default="")
     notify_email = models.EmailField(max_length=512, blank=True, null=True)
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
+
+
+class OrganizationMember(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, default="")
+
+
+class OrganizationMemberMonitor(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, default="")
+
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=17, blank=True, null=True)
+    name = models.CharField(max_length=17, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    notify_email = models.EmailField(max_length=512, blank=True, null=True)
+    days_sober = models.PositiveIntegerField(default=0, null=True)
+    sober_date = models.CharField(max_length=256, blank=True, null=True)
+    user_hash = models.CharField(max_length=256, blank=True, null=True)
+    source = models.CharField(max_length=500, default="", null=True)
+    stripe_subscription_id = models.CharField(max_length=256,
+                                              blank=True, null=True)
+    paying = models.BooleanField(default=False)
+    iap_blurb = models.TextField(default="")
+    org = models.ForeignKey(to=Organization,
+                            on_delete=models.CASCADE, default="")
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.user_hash = hashlib.sha1(
+                self.user.email.encode('utf-8')
+            ).hexdigest()
+        super(UserProfileInfo, self).save(*args, **kwargs)
