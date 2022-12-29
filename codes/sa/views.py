32ab416adf2d633fb6f6228f929b01f457799c1f
ARHPmabjcAttP <aaronorosen@gmail.com>
2022-12-29 02:42:54 -0500

    working on session points apis

--- codes/sa/views.py
+++ codes/sa/views.py
@@ -1,3 +1,4 @@
+import json
 import datetime
 from django.http import JsonResponse
 
@@ -10,6 +11,7 @@ from rest_framework.decorators import permission_classes
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.core.serializers import serialize
 
 
 @api_view(['POST'])
@@ -62,13 +64,36 @@ def get_distances(request):
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
 def get_session_stats(request):
-    session_points = SessionPoint.objects.filter().values_list()
-    for session_point in session_points:
-        print(session_point)
+    session_points = SessionPoint.objects.filter().order_by("-id")
 
+    sessions = Session.objects.filter(
+        device__key=request.GET.get("device_id")
+    )
+
+    session_response = {}
+
+    for session in sessions:
+        session_response['id'] = session.id
+
+        session_points = SessionPoint.objects.filter(
+            session=session
+        ).order_by("-id")
+
+        total_distance = 0
+        for i in range(0, len(session_points) - 1):
+            total_distance += get_distance(
+                session_points[i].latitude, session_points[i].longitude,
+                session_points[i + 1 ].latitude, session_points[i +1].longitude
+            )
+        session_response[session.id] = {"distance": total_distance}
+
+
+    session_points = serialize("json", session_points)
+    session_points = json.loads(session_points)
 
     return JsonResponse({'status': 'okay',
-                         'points': session_points}, safe=False)
+                         'device_id': request.GET.get("device_id"),
+                         'sessions_stats': session_response}, safe=False)
 
 
 
@@ -76,6 +101,7 @@ def get_session_stats(request):
 @authentication_classes([JWTAuthentication])
 def start(request):
     session = Session.objects.create()
+    session.save()
 
     # if device does not exist when session is started
     # it is created here
@@ -83,12 +109,14 @@ def start(request):
     device_id = request.data.get("device_id")
     print(device_id)
     print(type(device_id))
-
     device = Device.objects.filter(key=device_id).first()
     if not device:
         device = Device()
         device.key = device_id
         device.save()
+
+    session.device = device
+    session.save()
 
     return JsonResponse({'status': 'okay',
                          'session_id': session.id}, safe=False)
